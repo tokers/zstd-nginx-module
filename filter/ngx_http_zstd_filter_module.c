f928a03a8b76eecb0c3c408e05839f85b9309da5
tokers <zchao1995@gmail.com>
2023-09-28 11:02:58 +0800

    fix: avoid using deprecated ZSTD_initCStream_usingCDict if zstd version >= 1.5.0
    Signed-off-by: tokers <zhangchao.1995@bytedance.com>

--- filter/ngx_http_zstd_filter_module.c
+++ filter/ngx_http_zstd_filter_module.c
@@ -603,7 +603,25 @@ ngx_http_zstd_filter_create_cstream(ngx_http_request_t *r,
     /* TODO use the advanced initialize functions */
 
     if (zlcf->dict) {
+#if ZSTD_VERSION_NUMBER >= 10500
+        rc = ZSTD_CCtx_reset(cstream, ZSTD_reset_session_only);
+        if (ZSTD_isError(rc)) {
+            ngx_log_error(NGX_LOG_ALERT, r->connection->log, 0,
+                          "ZSTD_CCtx_reset() failed: %s",
+                          ZSTD_getErrorName(rc));
+            goto failed;
+        }
+
+        rc = ZSTD_CCtx_refCDict(cstream, zlcf->dict);
+        if (ZSTD_isError(rc)) {
+            ngx_log_error(NGX_LOG_ALERT, r->connection->log, 0,
+                          "ZSTD_CCtx_refCDict() failed: %s",
+                          ZSTD_getErrorName(rc));
+            goto failed;
+        }
+#else
         rc = ZSTD_initCStream_usingCDict(cstream, zlcf->dict);
+#endif
         if (ZSTD_isError(rc)) {
             ngx_log_error(NGX_LOG_ALERT, r->connection->log, 0,
                           "ZSTD_initCStream_usingCDict() failed: %s",
